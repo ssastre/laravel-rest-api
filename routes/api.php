525fd631b6763b8ca6647d1fbed7c78a68ef303b
Martin Ugarte <sandra_sastre@hotmail.com>
2022-08-15 09:41:54 -0500

    commit inicial

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::post('/reset', 'ResetController@reset');
+
+Route::get('/balance', 'BalanceController@show');
+
+Route::post('/event', 'EventController@store');
+
+
